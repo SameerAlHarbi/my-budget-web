348b3777bae591e747dec0c63276013966a240eb
Sameer Al Harbi <smrhrbi@gmail.com>
2020-02-16 02:02:08 +0300

    Initial Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BeneficiariesComponent } from './beneficiaries/beneficiaries.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { RelationsComponent } from './relations/relations.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent, pathMatch: 'full'},
+  {path:'beneficiaries', component: BeneficiariesComponent},
+  {path:'relations', component: RelationsComponent},
+  {path:'about', component: HomeComponent},
+  {path: 'not-found', component: ErrorPageComponent
+    , data: { errorType: 'not found', errorCode: 404, errorMessage: 'Page not found!'}},
+  {path: '**', redirectTo: 'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BeneficiariesComponent } from './beneficiaries/beneficiaries.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { RelationsComponent } from './relations/relations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BeneficiariesComponent,
+    ErrorPageComponent,
+    RelationsComponent
   ],
   imports: [
     BrowserModule,
